9f1026d6cccdd30d66c944472241972117d542ed
Maxime FRIESS <maxime.friess@etu.unistra.fr>
2021-12-23 20:06:35 +0100

    [event] Added price and validator calculation

--- src/models/Event.php
+++ src/models/Event.php
@@ -4,6 +4,7 @@ namespace Database\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Event extends Model
 {
@@ -39,5 +40,60 @@ class Event extends Model
     {
         return $this->belongsToMany(Person::class, 'event_person', 'event_id', 'person_id')
             ->using(EventPerson::class)->withTimestamps();
+    }
+
+    public function price($data, Person $person) {
+        $member = $person->member()->exists();
+        $price = $member ? $this->price_member : $this->price;
+
+        foreach($this->data as $dat) {
+            if ($dat["type"] == "boolean") {
+                if ($data[$dat["name"]] == true) {
+                    $price += $member ? $dat["price_member"] : $dat["price"];
+                }
+            } else if ($dat["type"] == "select") {
+                $good = false;
+                foreach($dat["values"] as $val) {
+                    if ($data[$dat["name"]] == $val["name"]) {
+                        $price += $member ? $val["price_member"] : $val["price"];
+                        $good = true;
+                        break;
+                    }
+                }
+                if (!$good)
+                    abort(400);
+            }
+        }
+        
+        return $price;
+    }
+
+    public function validator($sometimes = false)
+    {
+        $output = [];
+        $keys = [];
+        foreach ($this->data as $element) {
+            switch ($element["type"]) {
+                case "string":
+                    $output['data.' . $element["name"]] = ['required', 'string'];
+                    break;
+                case "boolean":
+                    $output['data.' . $element["name"]] = ['required', 'boolean'];
+                    break;
+                case "select":
+                    $values = [];
+                    foreach($element["values"] as $val) {
+                        $values[] = $val["name"];
+                    }
+                    $output['data.' . $element["name"]] = ['required', Rule::in($values)];
+                    break;
+            }
+            if ($sometimes) {
+                $output['data.' . $element["name"]][] = 'sometimes';
+            }
+            $keys[] = $element["name"];
+        }
+        $output["data"] = ['required', 'array:' . join(",", $keys)];
+        return $output;
     }
 }
